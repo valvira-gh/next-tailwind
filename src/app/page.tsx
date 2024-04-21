d63f8b40fea8dd9c6cccd99003cfc2a2258e1362
vivaldev_ <149679225+valvira-gh@users.noreply.github.com>
2024-04-21 15:33:35 +0300

    Working app with Next - TailwindCSS - Docker. Next adding Prisma and test db connection.

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const Home: React.FC = () => {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-6xl">Next - TailwindCSS - Docker</h1>
+    </main>
+  );
+};
+
+export default Home;
